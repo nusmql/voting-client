c62b2af028e464d5a67a191534e81a5e2e30ceaf
U-SG\Jack.Meng <Jack.Meng@SG217.sg.bwoil.com>
2016-12-06 11:42:59 +0800

    Add immmutable method into props

--- test/components/Voting_spec.jsx
+++ test/components/Voting_spec.jsx
@@ -6,9 +6,8 @@ import {
   Simulate
 } from 'react-addons-test-utils'
 
+import {List} from 'immutable'
 import Voting from '../../src/components/Voting'
-import Vote from '../../src/components/Vote'
-import Winner from '../../src/components/Winner'
 import {expect} from 'chai'
 
 describe('Voting', () => {
@@ -72,5 +71,46 @@ describe('Voting', () => {
     expect(winner.textContent).to.contain('Trainspotting');
   });
 
+  it('renders as a pure component', () => {
+    const pair = ['Trainspotting', '28 Days Later']
+    const container = document.createElement('div')
+    let component = ReactDOM.render(
+      <Voting pair={pair} />,
+      container
+    )
+
+    let firstButton = scryRenderedDOMComponentsWithTag(component, 'button')[0]
+    expect(firstButton.textContent).to.equal('Trainspotting')
+
+    pair[0] = 'Sunshine'
+    component = ReactDOM.render(
+      <Voting pair={pair} />,
+      container
+    )
+
+    firstButton = scryRenderedDOMComponentsWithTag(component, 'button')[0]
+    expect(firstButton.textContent).to.equal('Sunshine')
+  })
+
+  it('does update DOM when prop changes', () => {
+    const pair = List.of('Trainspotting', '28 Days Later')
+    const container = document.createElement('div')
+    let component = ReactDOM.render(
+      <Voting pair={pair} />,
+      container
+    )
+
+    let firstButton = scryRenderedDOMComponentsWithTag(component, 'button')[0];
+    expect(firstButton.textContent).to.equal('Trainspotting');
+
+    const newPair = pair.set(0, 'Sunshine')
+    component = ReactDOM.render(
+      <Voting pair={newPair} />,
+      container
+    )
+    firstButton = scryRenderedDOMComponentsWithTag(component, 'button')[0];
+    expect(firstButton.textContent).to.equal('Sunshine');
+
+  })
 
 })
